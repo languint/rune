beb68aaa7c666f0107bd57860ea70bf1480a6c90
languint <longuint@proton.me>
2025-06-24 17:55:07 -0500

    feat(parser): add nodes

--- rune-parser/src/lib.rs
+++ rune-parser/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod nodes;
 pub mod parser;
 pub mod tokens;
---
+++ rune-parser/src/nodes.rs
@@ -0,0 +1,30 @@
+pub enum Nodes {
+    Integer(i64),
+    Float(f64),
+    String(String),
+    Boolean(bool),
+    Identifier(String),
+}
+
+impl Nodes {
+    #[inline]
+    pub fn new_integer(value: i64) -> Self {
+        Nodes::Integer(value)
+    }
+    #[inline]
+    pub fn new_float(value: f64) -> Self {
+        Nodes::Float(value)
+    }
+    #[inline]
+    pub fn new_string(value: String) -> Self {
+        Nodes::String(value)
+    }
+    #[inline]
+    pub fn new_boolean(value: bool) -> Self {
+        Nodes::Boolean(value)
+    }
+    #[inline]
+    pub fn new_identifier(value: String) -> Self {
+        Nodes::Identifier(value)
+    }
+}
